7a6a0d574e4e9a42d326036806b75fd1562c9682
JunsuChoi <jsuya.choi@samsung.com>
2021-11-12 16:18:53 +0900

    hot fix

--- .github/workflows/memcheck_valgrind.sh
+++ .github/workflows/memcheck_valgrind.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "The GITHUB_TOKEN is required."
+	exit 1
+fi
+
 echo "Run Valgrind"
 echo "valgrind --leak-check=yes ./tvgUnitTests"
 cd ./build/test
